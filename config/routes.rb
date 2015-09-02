f4f0c3209deb1e701dabc081765b73439216e790
McCartney Bohn <mccartneybohn@yahoo.com>
2015-09-02 12:48:39 -0600

    added routes and new pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+   root 'pages#index'
 
-   get 'home#github'
+   get '/contacts' => 'contacts#index'
+
 
 
   # Example of regular route:
