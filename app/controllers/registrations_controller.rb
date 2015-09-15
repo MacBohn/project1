dd45c9737227a98c29c16cf4679b3e847fd88974
McCartney Bohn <mccartneybohn@yahoo.com>
2015-09-15 15:23:18 -0600

    fixed sign up error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,7 +18,7 @@ class RegistrationsController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 
